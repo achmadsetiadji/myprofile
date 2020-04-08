185685a150d1255bc96351c20220835d543fe1b6
achmadsetiadji <55737156+achmadsetiadji@users.noreply.github.com>
2020-04-08 22:06:40 +0700

    first commit

---
+++ aboutme.php
@@ -0,0 +1,7 @@
+<?php
+echo '<br>';
+echo 'Nama : Achmad Setiadji Nugroho <br>';
+echo 'Kelas : XI RPL 1 <br>';
+echo 'Asal Sekolah : SMK Taruna Bhakti Depok <br>';
+echo 'Alamat Rumah : Kp.Sindangkarsa No.12 RT 01/08, Sukamaju Baru, Tapos, Depok, Kode Pos 16455 <br>';
+?>
